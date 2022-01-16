8f30d42b8040f3a84eacd0e99897b5cbc6ed2f84
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-01-16 23:23:46 +0530

    commit

--- userfrom/src/components/Form.jsx
+++ userfrom/src/components/Form.jsx
@@ -4,6 +4,7 @@ import "./form.css";
 export const Form = () => {
   const ref = useRef(null);
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
   const [form, setForm] = useState({
     name: "",
     age: "",
@@ -16,10 +17,10 @@ export const Form = () => {
   });
   useEffect(() => {
     getData();
-  }, []);
+  }, [page]);
 
   const getData = () => {
-    fetch(`http://localhost:3001/users`)
+    fetch(`http://localhost:3001/users?_page=${page}&_limit=1`)
       .then((d) => d.json())
       .then((res) => setData(res));
   };
@@ -61,15 +62,16 @@ export const Form = () => {
       getData();
     });
   };
+  console.log("page", page)
   const salarySort = (val) => {
-    fetch(`http://localhost:3001/users?_sort=Salary&_order=${val}`)
+    fetch(`http://localhost:3001/users?_sort=salary&_order=${val}`)
       .then((e) => e.json())
       .then((e) => {
-        getData(e)
-        console.log(e);
+        console.log("sort", e);
+        setData(e)
       });
-    console.log("inside sort", data)
   };
+  console.log("inside sort", data)
   return (
     <>
       <div className="main">
@@ -144,6 +146,9 @@ export const Form = () => {
         </div>
         <div className="dataDiv">
           <Table list={data} />
+          <button onClick={() => setPage(page - 1)}>prev</button>
+          <button onClick={() => setPage(page + 1)}>next</button>
+          <br />
           <button onClick={() => salarySort("asc")}>Salary Low to High</button>
           <button onClick={() => salarySort("desc")}>Salary High to Low</button>
         </div>
